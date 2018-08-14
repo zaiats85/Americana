07d33baece02828fb72c2727294313cdae55e882
oleg zaiats <zaiats85@Sergiis-iMac.local>
2018-08-14 12:17:45 +0300

    create redux store

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux'
+import reducer from './reducers/index'
+
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension())
+  }
+}
+
+export const store = createStore(reducer);

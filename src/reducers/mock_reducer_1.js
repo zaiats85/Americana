2d589eda239bc3c1c6636e892ee08877b8ed9815
oleg zaiats <zaiats85@Sergiis-iMac.local>
2018-08-14 12:01:08 +0300

    add mock reducers

---
+++ src/reducers/mock_reducer_1.js
@@ -0,0 +1,5 @@
+export default function mock_reducer_1(state = {}, action) {
+  switch (action.type){
+    default: return state
+  }
+}
---
+++ src/reducers/mock_reducer_2.js
@@ -0,0 +1,5 @@
+export default function mock_reducer_2(state = {}, action) {
+  switch (action.type){
+    default: return state
+  }
+}
